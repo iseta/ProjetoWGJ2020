7343b0139b86c7065d3f99691be131e6d746465a
Iseta <isabelle.brandelero@gmail.com>
2020-08-25 21:18:54 -0300

    Sincronização com alguns dos áudios

---
+++ Assets/AudioControls.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControls : MonoBehaviour
+{
+    public AudioSource self;
+
+    public void AdjustVolume(bool i)
+    {
+        if (i)
+        {
+            StartCoroutine(FadeIn());
+        }
+        else
+        {
+            StartCoroutine(FadeOut());
+        }
+    }
+
+    private IEnumerator FadeIn()
+    {
+        float volume = 0.5f;
+        float volumeEndValue = 0;
+        while (volume >= volumeEndValue)
+        {
+            SetSoundVolume(ref volume, -1);
+            yield return null;
+        }
+        self.volume = volumeEndValue;
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float volume = 0;
+        float volumeEndValue = 0.5f;
+        while (volume <= volumeEndValue)
+        {
+            SetSoundVolume(ref volume, 1);
+            yield return null;
+        }
+        self.volume = volumeEndValue;
+    }
+
+    private void SetSoundVolume(ref float volume, int Dir)
+    {
+        self.volume = volume;
+        volume += Time.deltaTime * (1.0f / 1.0f) * (Dir);
+    }
+}
